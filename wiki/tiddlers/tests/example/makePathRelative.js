c3041f32273a44bacc0072cfe44be92a12a07ac5
lin onetwo <linonetwo012@gmail.com>
2025-02-26 22:19:36 +0800

    fix: test, result when is absolute should have file://

--- wiki/tiddlers/tests/example/makePathRelative.js
+++ wiki/tiddlers/tests/example/makePathRelative.js
@@ -12,15 +12,15 @@ describe('Path utils tests', function() {
     }
 
     test("/Users/me/something/file.png", "/Users/you/something/index.html", "../../me/something/file.png");
-    test("/Users/me/something/file.png", "/Users/you/something/index.html", "/Users/me/something/file.png", { useAbsoluteForNonDescendents: true });
+    test("/Users/me/something/file.png", "/Users/you/something/index.html", "file:///Users/me/something/file.png", { useAbsoluteForNonDescendents: true });
     test("/Users/me/something/else/file.png", "/Users/me/something/index.html", "else/file.png");
     test("/Users/me/something/file.png", "/Users/me/something/new/index.html", "../file.png");
-    test("/Users/me/something/file.png", "/Users/me/something/new/index.html", "/Users/me/something/file.png", { useAbsoluteForNonDescendents: true });
+    test("/Users/me/something/file.png", "/Users/me/something/new/index.html", "file:///Users/me/something/file.png", { useAbsoluteForNonDescendents: true });
     test("/Users/me/something/file.png", "/Users/me/something/index.html", "file.png");
     test("/Users/jeremyruston/Downloads/Screenshot 2020-10-18 at 15.33.40.png", "/Users/jeremyruston/git/Jermolene/TiddlyWiki5/editions/prerelease/output/index.html", "../../../../../../Downloads/Screenshot%202020-10-18%20at%2015.33.40.png");
     test("/Users/me/nothing/image.png", "/Users/me/something/a/b/c/d/e/index.html", "../../../../../../nothing/image.png");
     test("C:\\Users\\me\\something\\file.png", "/C:/Users/me/something/index.html", "file.png", { isWindows: true });
     test("\\\\SHARE\\Users\\me\\something\\file.png", "/SHARE/Users/me/somethingelse/index.html", "../something/file.png", { isWindows: true });
-    test("\\\\SHARE\\Users\\me\\something\\file.png", "/C:/Users/me/something/index.html", "/SHARE/Users/me/something/file.png", { isWindows: true });
+    test("\\\\SHARE\\Users\\me\\something\\file.png", "/C:/Users/me/something/index.html", "file:///SHARE/Users/me/something/file.png", { isWindows: true });
   });
 });
